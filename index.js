b2f4eff6d3a812a66ffd39fb60f75c8c01895a0e
deparlak <deparlak@googlemail.com>
2014-09-04 18:46:52 +0200

    - updated observer to use couchbase bucket directly for view access.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var PouchDB = require('pouchdb');
 var Worker = require('de.htwg.seapal.worker');
 var nconf = require('nconf');
+var couchbase = require('couchbase');
+
+var config = { host :  [ "localhost:8091" ],
+        bucket : "sync_gateway"
+    };
+
 
 //
 // Setup nconf to use (in-order):
@@ -16,25 +22,93 @@ nconf.argv()
 var server = nconf.get("server");
 // set user object
 var user = nconf.get("user");
-// reference to db
-var db;
+// get couchbase config
+var couchbaseConf = nconf.get("couchbase");
+// reference to pouchdb handle
+var pouchdb;
+// reference to couchbase
+var bucket;
+// reference to the view
+var viewQuery;
+// variable which will be set if we should exit
+var exit = false;
+// timer which calls query cyclic
+var timer;
+// the timeout after which to execute the query again
+var timeout = 10000;
+
+var opts = {
+        limit           :   300,
+        group           :   true,
+        group_level     :   6,
+        reduce          :   true,
+        stale           :   "false"
+    };
+    
+// handle the exit event
+process.on('exit', function(code) {
+    if (worker) {
+        worker.close();
+    }
+});
+
+// handle termination of program
+process.on('SIGINT', function() {
+    console.log('TripSimulator got SIGINT.');
+    clearTimeout(timer);
+    process.exit(0);
+});
 
 // start new worker
 var worker = new Worker(server, user, function(err, response) {
     if (err) {
-        console.log("GOT ERROR");
         console.log(err);
         return;
     }
     
     console.log("Observer is running");
-    // save db handle
-    db = response;
-    // listen on db changes, to trigger document summarized on a update.
-    db.changes({since : 'now', live : true})
-        .on('change', function (info) {
-            console.log(info);
-        }).on('error', function (err) {
-            console.log(err);
+    // save pouchdb handle
+    pouchdb = response;
+   
+    bucket = new couchbase.Connection(couchbaseConf, function(err) {
+        if (err) {
+            // Failed to make a connection to the Couchbase cluster.
+            throw err;
+        }
+        
+        // set the view which we like to get
+        viewQuery = bucket.view('geohash', 'activeTrips');
+        // set the query parameter
+        setQueryParameter();
+        // query in a endless loop
+        queryLoop();
+    });
+    
+    // query the view.
+    var queryLoop = function () {
+        clearTimeout(timer);
+        // execute query now
+        viewQuery.query(opts, function(err, results) {
+            for(i in results) {
+                updateStatistic(results[i].key, results[i].value);
+            }
+            // set the query parameter for the next run
+            setQueryParameter();
+            // init the timeout.
+            timer = setTimeout(queryLoop, timeout); 
         });
+    }
+    
+    // set parameters for the view.
+    var setQueryParameter = function () {
+        // get the time now.
+        var now = new Date();
+        // set the actual time as the start key.
+        opts.startkey = [now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes()];
+    }; 
+    
+    // update the statistic, by generating a document with the summarized value from the view
+    var updateStatistic = function (key, value) {
+        console.log("update statistic for : "+key + " : "  + value);
+    };
 });
