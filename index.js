fc1ba7baf9bd9f2a89742feb78ddd3b009c4ac10
deparlak <deparlak@googlemail.com>
2014-09-06 15:36:46 +0200

    - added inWorkQueue which holds actually calculating values to prevent multiple updates.

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ var exit = false;
 var timer;
 // the timeout after which to execute the query again
 var timeout = 10000;
+// work queue to add documents which are currently updated
+var inWorkQueue = [];
 
 var opts = {
         limit           :   300,
@@ -109,6 +111,60 @@ var worker = new Worker(server, user, function(err, response) {
     
     // update the statistic, by generating a document with the summarized value from the view
     var updateStatistic = function (key, value) {
-        console.log("update statistic for : "+key + " : "  + value);
+        // console.log("update statistic for : "+key + " : "  + value);
+        // extract the hash, which is at last position after the date
+        var geohash = key[key.length - 1];
+
+        var doc = {};
+        doc.geohash = geohash;
+        doc.type = 'publishGeohash';
+        doc._id = 'publishGeohash/' + geohash;
+        doc._rev = null;
+        doc.channels = ['geohash-' + geohash];
+        doc.count = value;
+        
+        // if document is already in inWorkQueue, than do no update.
+        if (-1 !== inWorkQueue.indexOf(doc._id)) {
+            return;
+        }
+        // add document in inWorkQueue, to avoid multiple doc creations.
+        inWorkQueue.push(doc._id);
+        
+        console.log(new Date().toISOString() + " : Documents in work queue : " + inWorkQueue.length);
+        console.log("Proccess : "+geohash);
+        
+        // get the document and update it
+        pouchdb.get(doc._id, function (err, response) {
+            // check if the document could not be get. If we have a 404, no initial document existed yet
+            if (err && err.status != 404) {
+                throw new Error(err);
+            }
+
+            // check if there was a _rev returned
+            if (response && response._rev) {
+                doc._rev = response._rev;
+            }
+            
+            // update it now.
+            pouchdb.put(doc, function(err, response) {
+                if (err) {
+                    throw new Error(err);
+                }
+                
+                // check if document is in in work queue and remove it
+                var index = inWorkQueue.indexOf(doc._id);
+                if (-1 !== index) {
+                    inWorkQueue.splice(index, 1);
+                }
+                
+                console.log(new Date().toISOString() + " : Documents in work queue : " + inWorkQueue.length);
+                
+                // check if there was a _rev returned
+                if (!response || !response.rev) {
+                    throw new Error("Got no rev after put");
+                }
+            });
+        });
+        
     };
 });
