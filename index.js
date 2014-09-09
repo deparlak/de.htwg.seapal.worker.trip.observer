b6eaa53e06d55fb7eee3e368a472e708882bd4ee
deparlak <deparlak@googlemail.com>
2014-09-09 09:07:54 +0200

    - added method to summarize results.

--- index.js
+++ index.js
@@ -33,14 +33,18 @@ var viewQuery;
 // variable which will be set if we should exit
 var exit = false;
 // timer which calls query cyclic
-var timer;
+var queryTimer;
 // the timeout after which to execute the query again
-var timeout = 10000;
+var queryTimeout = 10000;
 // work queue to add documents which are currently updated
 var inWorkQueue = [];
+// the maximum resolution of geohash values
+var MAX_GEOHASH_RESOLUTION = 3;
+// the possible max geohash resolution
+var REAL_MAX_GEOHASH_RESOLUTION = 9;
 
 var opts = {
-        limit           :   300,
+        limit           :   10000,
         group           :   true,
         group_level     :   6,
         reduce          :   true,
@@ -57,7 +61,7 @@ process.on('exit', function(code) {
 // handle termination of program
 process.on('SIGINT', function() {
     console.log('TripSimulator got SIGINT.');
-    clearTimeout(timer);
+    clearTimeout(queryTimer);
     process.exit(0);
 });
 
@@ -88,16 +92,20 @@ var worker = new Worker(server, user, function(err, response) {
     
     // query the view.
     var queryLoop = function () {
-        clearTimeout(timer);
+        clearTimeout(queryTimer);
         // execute query now
         viewQuery.query(opts, function(err, results) {
-            for(i in results) {
-                updateStatistic(results[i].key, results[i].value);
+            console.log(results.length);
+            var documents = prepareResults(results);
+               
+            for(i in documents) {
+                updateStatistic(documents[i]);
             }
+            
             // set the query parameter for the next run
             setQueryParameter();
-            // init the timeout.
-            timer = setTimeout(queryLoop, timeout); 
+            // init the timeout to call the queryLoop later.
+            queryTimer = setTimeout(queryLoop, queryTimeout); 
         });
     }
     
@@ -109,19 +117,70 @@ var worker = new Worker(server, user, function(err, response) {
         opts.startkey = [now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes()];
     }; 
     
-    // update the statistic, by generating a document with the summarized value from the view
-    var updateStatistic = function (key, value) {
-        // console.log("update statistic for : "+key + " : "  + value);
-        // extract the hash, which is at last position after the date
-        var geohash = key[key.length - 1];
+    // prepare the result, which mean that we use run through the documents and do some summation
+    var prepareResults = function (queryResult) {
+        var docs = [];
+        var sum = false;
+        var index;
+        var now = new Date().toISOString();
+        // run through all results
+        for(var i = 0; i < queryResult.length; i++) {
+            // save the key of the queryResult
+            key = queryResult[i].key;
+            geohash = key[key.length - 1]; 
+            // save the value of the queryResult
+            value = queryResult[i].value;
+            // is sum disabled
+            if (!sum) {
+                sum = true;
+               // console.log("SUM "+geohash);
+                // create a new doc
+                var doc = {};
+                doc.owner = user.email;
+                doc.geohash = geohash;
+                doc.type = 'publishGeohash';
+                doc._id = 'publishGeohash/' + geohash;
+                doc._rev = null;
+                doc.count = value;
+                tmpCount = value;
+                childDepth = 0;
+                doc.date = now;
+                doc.child = [];
+                // save index value
+                index = i;
+                // continue with next document
+                continue;
+            }
+            // if it a child document
+            if (geohash.length - 1 == doc.geohash.length || (geohash.length == REAL_MAX_GEOHASH_RESOLUTION && doc.geohash.length == MAX_GEOHASH_RESOLUTION)) {
+                // sum is enabled, add, untill all child documents are added.
+                // All child documents are added, when the sum of the child values is equal to the parent sum.
+                tmpCount -= value;
 
-        var doc = {};
-        doc.geohash = geohash;
-        doc.type = 'publishGeohash';
-        doc._id = 'publishGeohash/' + geohash;
-        doc._rev = null;
-        doc.count = value;
-        doc.date = new Date().toISOString();
+                // store the geohash of the child to the document.
+                doc.child.push(geohash);
+                // all child docs added?
+                if (0 == tmpCount) {
+                    // set sum to false, to store next document with childs.
+                    sum = false;
+                    // push to all documents store.
+                    docs.push(doc);
+                    // if the childs added to this document where not the childs of the maximum geohash resolution
+                    if (doc.geohash.length < MAX_GEOHASH_RESOLUTION) {
+                        // set counter back, to check the index document, to count after this one.
+                        i = index;
+                    }
+                }
+            }
+        }
+        return docs;
+    }
+    
+    
+    // update the statistic, by generating a document with the summarized value from the view
+    var updateStatistic = function (doc) {
+         console.log("update statistic for : "+key + " : "  + value);
+        // extract the hash, which is at last position after the date
         
         // if document is already in inWorkQueue, than do no update.
         if (-1 !== inWorkQueue.indexOf(doc._id)) {
