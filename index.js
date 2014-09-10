2adf34f990f35caaf4fe3045dffc1516bdf3166e
deparlak <deparlak@googlemail.com>
2014-09-10 08:41:03 +0200

    - moved config from hard coded to config.json - use of another view.

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ var Worker = require('de.htwg.seapal.worker');
 var nconf = require('nconf');
 var couchbase = require('couchbase');
 
-var config = { host :  [ "localhost:8091" ],
-        bucket : "sync_gateway"
-    };
-
-
 //
 // Setup nconf to use (in-order):
 //   1. Command-line arguments
@@ -24,6 +19,8 @@ var server = nconf.get("server");
 var user = nconf.get("user");
 // get couchbase config
 var couchbaseConf = nconf.get("couchbase");
+// get view config
+var view = nconf.get("views")[0];
 // reference to pouchdb handle
 var pouchdb;
 // reference to couchbase
@@ -38,19 +35,8 @@ var queryTimer;
 var queryTimeout = 10000;
 // work queue to add documents which are currently updated
 var inWorkQueue = [];
-// the maximum resolution of geohash values
-var MAX_GEOHASH_RESOLUTION = 3;
-// the possible max geohash resolution
-var REAL_MAX_GEOHASH_RESOLUTION = 9;
 
-var opts = {
-        limit           :   10000,
-        group           :   true,
-        group_level     :   6,
-        reduce          :   true,
-        stale           :   "false"
-    };
-    
+
 // handle the exit event
 process.on('exit', function(code) {
     if (worker) {
@@ -68,8 +54,8 @@ process.on('SIGINT', function() {
 // start new worker
 var worker = new Worker(server, user, function(err, response) {
     if (err) {
-        console.log(err);
-        return;
+        // querry failed
+        throw err;
     }
     
     console.log("Observer is running");
@@ -81,9 +67,8 @@ var worker = new Worker(server, user, function(err, response) {
             // Failed to make a connection to the Couchbase cluster.
             throw err;
         }
-        
         // set the view which we like to get
-        viewQuery = bucket.view('geohash', 'activeTrips');
+        viewQuery = bucket.view(view.design, view.name);
         // set the query parameter
         setQueryParameter();
         // query in a endless loop
@@ -94,14 +79,16 @@ var worker = new Worker(server, user, function(err, response) {
     var queryLoop = function () {
         clearTimeout(queryTimer);
         // execute query now
-        viewQuery.query(opts, function(err, results) {
-            console.log(results.length);
-            var documents = prepareResults(results);
-               
-            for(i in documents) {
-                updateStatistic(documents[i]);
+        viewQuery.query(view.opts, function(err, results) {
+            if (err) {
+                console.error(err);
+                // querry failed
+                throw err;
             }
-            
+            if (results.length > 0) {
+                var doc = prepareResults(results);
+                updateStatistic(doc);
+            }
             // set the query parameter for the next run
             setQueryParameter();
             // init the timeout to call the queryLoop later.
@@ -114,74 +101,37 @@ var worker = new Worker(server, user, function(err, response) {
         // get the time now.
         var now = new Date();
         // set the actual time as the start key.
-        opts.startkey = [now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes()];
+        view.opts.startkey = [now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), view.opts.startkey];
+        // set the actual time as the start key.
+        view.opts.endkey = [now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), view.opts.endkey];
     }; 
     
     // prepare the result, which mean that we use run through the documents and do some summation
     var prepareResults = function (queryResult) {
-        var docs = [];
-        var sum = false;
-        var index;
+        var doc = {};
+        doc.owner = user.email;
+        doc.startkey = view.opts.startkey;
+        doc.endkey = view.opts.endkey;
+        doc.type = 'publishGeohash';
+        doc._id = 'publishGeohash/' + view.opts.startkey;
+        doc.sum = queryResult.length;
+        doc._rev = null;
+        doc.boats = {};
         var now = new Date().toISOString();
+        doc.date = now;
+
         // run through all results
         for(var i = 0; i < queryResult.length; i++) {
-            // save the key of the queryResult
-            key = queryResult[i].key;
-            geohash = key[key.length - 1]; 
-            // save the value of the queryResult
-            value = queryResult[i].value;
-            // is sum disabled
-            if (!sum) {
-                sum = true;
-               // console.log("SUM "+geohash);
-                // create a new doc
-                var doc = {};
-                doc.owner = user.email;
-                doc.geohash = geohash;
-                doc.type = 'publishGeohash';
-                doc._id = 'publishGeohash/' + geohash;
-                doc._rev = null;
-                doc.count = value;
-                tmpCount = value;
-                childDepth = 0;
-                doc.date = now;
-                doc.child = [];
-                // save index value
-                index = i;
-                // continue with next document
-                continue;
-            }
-            // if it a child document
-            if (geohash.length - 1 == doc.geohash.length || (geohash.length == REAL_MAX_GEOHASH_RESOLUTION && doc.geohash.length == MAX_GEOHASH_RESOLUTION)) {
-                // sum is enabled, add, untill all child documents are added.
-                // All child documents are added, when the sum of the child values is equal to the parent sum.
-                tmpCount -= value;
-
-                // store the geohash of the child to the document.
-                doc.child.push(geohash);
-                // all child docs added?
-                if (0 == tmpCount) {
-                    // set sum to false, to store next document with childs.
-                    sum = false;
-                    // push to all documents store.
-                    docs.push(doc);
-                    // if the childs added to this document where not the childs of the maximum geohash resolution
-                    if (doc.geohash.length < MAX_GEOHASH_RESOLUTION) {
-                        // set counter back, to check the index document, to count after this one.
-                        i = index;
-                    }
-                }
-            }
+            geohash = queryResult[i].key[queryResult[i].key.length - 1];
+            boat = queryResult[i].value;
+            doc.boats[boat] = geohash;
         }
-        return docs;
+        return doc;
     }
     
     
     // update the statistic, by generating a document with the summarized value from the view
-    var updateStatistic = function (doc) {
-         console.log("update statistic for : "+key + " : "  + value);
-        // extract the hash, which is at last position after the date
-        
+    var updateStatistic = function (doc) {        
         // if document is already in inWorkQueue, than do no update.
         if (-1 !== inWorkQueue.indexOf(doc._id)) {
             return;
@@ -190,7 +140,7 @@ var worker = new Worker(server, user, function(err, response) {
         inWorkQueue.push(doc._id);
         
         console.log(new Date().toISOString() + " : Documents in work queue : " + inWorkQueue.length);
-        console.log("Proccess : "+geohash);
+        console.log("Proccess : " + view.opts.startkey + " - " + view.opts.endkey);
         
         // get the document and update it
         pouchdb.get(doc._id, function (err, response) {
