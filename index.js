8e6461754221153edbd49c8197dd207cda786d43
deparlak <deparlak@googlemail.com>
2014-09-22 00:18:58 +0200

    - improved observer to be able to use the view range

--- index.js
+++ index.js
@@ -31,16 +31,16 @@ var viewQuery;
 var exit = false;
 // timer which calls query cyclic
 var queryTimer;
-// work queue to add documents which are currently updated
-var inWorkQueue = [];
-// range of valid geohash values.
-var validGeohashChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-// variable to store the last insert document, to push it again after it is obsolete
-var oldDoc;
 // max repeations after a timeout
 var maxTimeoutErrorRetries = 4;
 // counter for error, because of timeout
 var timeoutErrorCounter = 0;
+// document which will be published
+var publishGeohash = {};
+publishGeohash.owner = user.email;
+publishGeohash.type = 'publishGeohash';
+publishGeohash._id = user.email + '/publishGeohash';
+publishGeohash._rev = null;
 
 // handle the exit event
 process.on('exit', function(code) {
@@ -75,8 +75,21 @@ var worker = new Worker(server, user, function(err, response) {
         viewQuery = bucket.view(view.design, view.name);
         // set the query parameter
         setQueryParameter();
-        // query in a endless loop
-        queryLoop();
+        
+        // fetch the latest doc, which will be updated on new data
+        pouchdb.get(publishGeohash._id, function(err, response) {
+            // if we get an error, which is not because of a missing doc 
+            // (missing doc is ok if it's the first created document by this user)
+            if (err && err.status !== 404) {
+                throw new Error(err);
+            } else if (err && err.status === 404){
+                publishGeohash._rev = null;
+            } else {
+                publishGeohash._rev = response._rev;
+            }
+            // query in a endless loop
+            queryLoop();
+        });
     });
     
     // query the view.
@@ -89,10 +102,10 @@ var worker = new Worker(server, user, function(err, response) {
                 throw new Error(err);
             }
             if (results.length > 0) {
-                var doc = prepareResults(results);
-                updateStatistic(doc);
+                prepareResults(results);
+                updateStatistic();
             }
-            console.log("Querry result with "+results.length+ " documents.");
+            console.log("Querry : " + view.opts.startkey + " - " + view.opts.endkey+" "+results.length+ " documents");
             // set the query parameter for the next run
             setQueryParameter();
             // init the timeout to call the queryLoop later.
@@ -105,55 +118,48 @@ var worker = new Worker(server, user, function(err, response) {
         // get the time now.
         var now = new Date();
         // set the actual time as the start key.
-        view.opts.startkey = [[now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes()]];
+        view.opts.startkey = [now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes()].concat(view.settings.channelStart);
         // set the end key 1 minute later. Note that at the borders (from minute to minute, minute to hour, hour to hour, etc it is possible that not all data is available).
-        view.opts.endkey = [[now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes() + 1]];
+        view.opts.endkey = [now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes() + 1].concat(view.settings.channelEnd);
     }; 
     
     // prepare the result, which mean that we use run through the documents and do some summation
     var prepareResults = function (queryResult) {
-        var doc = {};
         var now = new Date().toISOString();
-        doc.date = now;
-        doc.owner = user.email;
-        doc.type = 'publishGeohash';
-        doc._id = user.email + '/publishGeohash/' + now;
-        doc.sum = queryResult.length;
-        doc._rev = null;
-        doc.boats = {};
+        publishGeohash.date = now;
+        publishGeohash.sum = queryResult.length;
+        publishGeohash.boats = {};
         // add the channels values, to which we should map this document.
-        doc.channels = view.channels;
+        publishGeohash.channels = view.settings.channels;
 
         // run through all results
         for(var i = 0; i < queryResult.length; i++) {
-            var geohash = queryResult[i].value.geohash;
-            var boat = queryResult[i].key[queryResult[i].key.length - 1];
-            doc.boats[boat] = geohash;
+            // key[0] - key[4] date in year,month,day,hour,minute
+            // key[5] - key[13] geohash
+            // key[14] user id
+            var boat = queryResult[i].key[14];
+            var geohash = "";
+            for (var j = 5; j < 14; j++) {
+                geohash += queryResult[i].key[j];
+            }
+            console.log(geohash);
+            console.log(boat);
+            publishGeohash.boats[boat] = geohash;
         }
-        return doc;
     }
     
     
     // update the statistic, by generating a document with the summarized value from the view
-    var updateStatistic = function (doc) {        
-        // if document is already in inWorkQueue, than do no update.
-        if (-1 !== inWorkQueue.indexOf(doc._id)) {
-            return;
-        }
-        // add document in inWorkQueue, to avoid multiple doc creations.
-        inWorkQueue.push(doc._id);
-        
-        console.log("Proccess : " + view.opts.startkey + " - " + view.opts.endkey);
-        
+    var updateStatistic = function () {
         // update it now.
-        pouchdb.put(doc, function(err, response) {
+        pouchdb.put(publishGeohash, function(err, response) {
             if (err) {
                 timeoutErrorCounter++;
                 // repeat after a timeout, if not max repeats are reached.
                 if (maxTimeoutErrorRetries > timeoutErrorCounter) {
                     console.log(err);
                     console.log("Retry after " + timeoutErrorCounter + " calls. "+err);
-                    timer = setTimeout(sumulatePosition, timeout);
+                    timer = setTimeout(updateStatistic, view.settings.queryTimeout);
                     return;
                 }
                 // max retries reached.
@@ -164,33 +170,14 @@ var worker = new Worker(server, user, function(err, response) {
                 // no error occurred, so set counter back
                 timeoutErrorCounter = 0;
             }
-
-            if (oldDoc) {
-                pouchdb.put(oldDoc, function(err, response) {
-                    if (err) {
-                        console.log("Error while updating obsolete document : "+err);
-                    } else {
-                        console.log("oldDoc is now obsolete!");
-                    }
-                });
-            }
-            
-            // ok document successfully stored, store a reference to the document to update it after it is obsolete
-            // it is obsolete when we push another document.
-            oldDoc = doc;
-            oldDoc.obsolete = true;
-            oldDoc._rev = response.rev;
-            
-            // check if document is in in work queue and remove it
-            var index = inWorkQueue.indexOf(doc._id);
-            if (-1 !== index) {
-                inWorkQueue.splice(index, 1);
-            }
             
             // check if there was a _rev returned
             if (!response || !response.rev) {
                 throw new Error("Got no rev after put");
             }
+            
+            // store _rev
+            publishGeohash._rev = response.rev;
         });
         
     };
